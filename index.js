831e1a11f2866ff29d9904794b0c943d4b57636d
Skraftz <davekenkent@gmail.com>
2024-06-13 10:47:19 -0400

    First Lines

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 function reqListener() {
     console.log(this.responseText);
+    var firstLine = text.split('\n').shift(); 
+    console.log(firstLine)
   }
   
   const req = new XMLHttpRequest();
   req.addEventListener("load", reqListener);
-  req.open("GET", "file.txt");
+  req.open("GET", "file.txt"[1]);
   req.send();
   
