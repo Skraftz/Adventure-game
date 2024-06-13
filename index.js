a7b4a90909a2ee5d8b60ee27c2a811f1428495ed
Skraftz <davekenkent@gmail.com>
2024-06-13 11:07:28 -0400

    v

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-var text;
+var text = "string";
+var line = "i";
+
 function reqListener() {
     text = this.responseText;
-  }
-  
-  const req = new XMLHttpRequest();
-  req.addEventListener("load", reqListener);
-  req.open("GET", "file.txt"[1]);
-  req.send();
-  
+};
+
+function getNextParagraph() {
+    text.replace(line, "");
+}
+
+function nameItLater(string) {
+    switch (string) {
+        case "H":
+            getNextParagraph()
+            document.querySelector("h1").innerHTML = "This is a test";
+            getNextParagraph()
+            document.querySelector("p").innerHTML = "Another Test.";
+    }
+};
+
+
+
+const req = new XMLHttpRequest();
+req.addEventListener("load", reqListener);
+req.open("GET", "file.txt"[1]);
+req.send();
+
