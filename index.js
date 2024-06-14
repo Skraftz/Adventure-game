11fba9446d00f41a8e635bc4eafdca38698f14d4
Skraftz <davekenkent@gmail.com>
2024-06-14 08:39:30 -0400

    thing

--- index.js
+++ index.js
@@ -3,34 +3,18 @@ let selector = "p";
 let pin = 0;
 
 
+function button(number, src, alt) {
+    document.getElementById(number).src = src;
+    document.getElementById(number).alt = alt;
+}
 
-async function getText() { //self explainitory
-    let response = await fetch('https://skraftz.github.io/Adventure-game/file.txt');
-    let sec = await response.text();
-    text = sec.split("\n");
-    console.log(text);
-};
 
-function nameItLater(x) {
-    switch (text[pin]) {
-        case "H": //Selects header 1
-            selector = "h1";
-            break;
-        case "I": // Replaces image and alt 
-            pin += 1;
-            document.querySelector("img").src = text[pin];
-            pin += 1;
-            document.querySelector("img").alt = text[pin];
-            break;
-        case "p":
-            selector = "p";
-            break;
-        case "O":
-            pin += 1;
+function ToggleButtons(Watch) {
+            document.getElementById("1").style.display = "none";
             document.getElementById("2").style.display = "none";
             document.getElementById("3").style.display = "none";
             document.getElementById("4").style.display = "none";
-            switch (text[pin]) {
+            switch (Watch) {
                 case "4": 
                     document.getElementById("2").style.display = "block";
                     document.getElementById("3").style.display = "block";
@@ -46,15 +30,77 @@ function nameItLater(x) {
                 default:
                     break;
                 }; 
+}
+
+
+
+
+
+
+
+
+
+
+async function getText() { //self explainitory
+    let response = await fetch('https://skraftz.github.io/Adventure-game/file.txt');
+    let sec = await response.text();
+    text = sec.split("\n");
+    console.log(text);
+};
+
+
+
+
+
+function nameItLater() {
+    switch (text[pin]) {
+        case "H": //Selects header 1
+            selector = "h1";
+            pin += 1; nameItLater();
+            break;
+        case "I": // Replaces image and alt 
+            pin += 1;
+            document.querySelector("img").src = text[pin];
+            pin += 1;
+            document.querySelector("img").alt = text[pin];
+            pin += 1; nameItLater();
+            break;
+        case "p":
+            selector = "p";
+            pin += 1; nameItLater();
+            break;
+        case "O":
+            pin += 1; ToggleButtons(text[pin]);//show buttons
+            let rep = Number(text[pin]);
+            let amm = 0;
+            while (rep > amm);
+            amm += 1;
+            pin += 1;
+            document.getElementById(String(amm)).innerHTML = text[pin];
+            pin += 1;
+            document.getElementById(String(amm)).className = text[pin];
+
+        //Show buttons//
+            
             break;
         default:
-            document.querySelector(selector).innerHTML = x;
+            document.querySelector(selector).innerHTML = text[pin];
+            
+            pin += 1; nameItLater();
             break;
         
     };
-    pin += 1;
 };
 
 console.log("I AM GOING TO BREAK");
 getText();
 
+
+document.querySelectorAll('button').forEach(cell => {
+    cell.addEventListener('click', function() {
+      let cellId = this.id;
+      let theclass = this.className;
+      console.log(theclass);
+      // Call your function with the cellId variable
+    });
+  });
